4da3301bb658bf1232d14a0a90b5d441506716b2
DevSergen <Sergen.Selcik@hotmail.com>
2024-06-12 21:09:43 +0200

    about me put in main-page component and arrow container done

--- src/app/main-page/about-me/about-me.component.ts
+++ src/app/main-page/about-me/about-me.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+  firstHover = true;
 
+  onHover() {
+    if (this.firstHover) {
+      this.firstHover = false;
+    }
+  }
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../shared/header/header.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { MenuComponent } from './menu/menu.component';
-import { AboutMeComponent } from '../about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboveTheFoldComponent, MenuComponent, AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, AboveTheFoldComponent, MenuComponent, AboutMeComponent , MySkillsComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
