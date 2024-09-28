667978b44db69e7f705014adc4f80d52c3cba425
DevSergen <Sergen.Selcik@hotmail.com>
2024-09-28 21:08:22 +0200

    changed some aos animations at portfolio sections for the  mobile version

--- src/app/main-page/portfolio/projects/projects.component.ts
+++ src/app/main-page/portfolio/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { ProjectsService } from '../../../services/projects.service';
 import { CommonModule } from '@angular/common';
 import { TranslationService } from '../../../services/translation.service';
@@ -16,4 +16,22 @@ import { TranslateModule } from '@ngx-translate/core';
 export class ProjectsComponent {
   projects = inject(ProjectsService).projects;
   translate = inject(TranslationService);
+
+  screenWidth: number = window.innerWidth;
+
+  constructor() {
+    this.onResize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  getAosAttribute(swapcontent: boolean): string {
+    if (this.screenWidth <= 1020) {
+      return 'fade-down';
+    }
+    return swapcontent ? 'fade-down-left' : 'fade-down-right';
+  }
 }
